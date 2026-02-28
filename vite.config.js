97750490f9f01b3c688e3cd4fd5c53b639708e5d
Nikita Kudenikov <kudenikov.92@gmail.com>
2026-02-28 14:25:02 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/irinadan-react/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'gh' ? '/irinadan-react/' : '/',
+    plugins: [react()],
+  }
 })
